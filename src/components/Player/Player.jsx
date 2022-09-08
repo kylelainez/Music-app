b044f081f1535873513b7b87dac40dce0dd370ac
kylelainez <kyleadrianlainez@gmail.com>
2022-09-07 19:58:28 -0700

    Configure files needed for the App

---
+++ src/components/Player/Player.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Player.scss';
+
+const Player = () => (
+	<div className='player-container'>
+		<h1>Player</h1>
+	</div>
+);
+
+export default Player;
---
+++ src/components/Song/Song.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Song.scss';
+
+const Song = () => (
+	<div className='song-container'>
+		<h1>Song</h1>
+	</div>
+);
+
+export default Song;
